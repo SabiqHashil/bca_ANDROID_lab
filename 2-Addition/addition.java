9f33702226ae4ec587e75c1a333731d31e5111f5
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-01-26 23:43:00 +0530

    2 added

---
+++ 2-Addition/addition.java
@@ -0,0 +1,43 @@
+package com.example.twosum;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    EditText firstNumber;
+    EditText secondNumber;
+    TextView addResult;
+    Button btnAdd;
+    double num1, num2, sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstNumber = (EditText) findViewById(R.id.txtNumber1);
+        secondNumber = (EditText) findViewById(R.id.txtNumber2);
+        addResult = (TextView) findViewById(R.id.txtResult);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                num1 = Double.parseDouble(firstNumber.getText().toString());
+                num2 = Double.parseDouble(secondNumber.getText().toString());
+                sum = num1 + num2;
+                addResult.setText(Double.toString(sum));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
