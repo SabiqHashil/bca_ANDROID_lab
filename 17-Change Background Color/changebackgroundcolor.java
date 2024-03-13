638b790856184eb8e7381004f4656de6c4cfef18
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-14 01:06:02 +0530

    added

---
+++ 17-Change Background Color/changebackgroundcolor.java
@@ -0,0 +1,29 @@
+package com.gems.pqr;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity {
+    Button b1;
+    RelativeLayout rel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        rel = (RelativeLayout) findViewById(R.id.r1);
+        b1.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub rel.setBackgroundColor(Color.BLUE);
+            }
+        });
+    }
+}
